79b48a42fa5dbb81ff505261373f903680e25563
John W. Clark <john.wilbur.clark@gmail.com>
2014-04-29 16:43:12 -0500

    cleaned up code per jshint

--- server.js
+++ server.js
@@ -8,6 +8,8 @@
  *
  */
 
+'use strict';
+
 var journey = require('journey');
 var http = require('http');
 
@@ -18,55 +20,55 @@ var portNum = 8303;
 console.log( 'running on localhost:' + portNum );
 
 var testData = {
-    "name": "Neo",
-    "agents": ["Smith", "Brown", "Jones" ],
-    "spoon": false
-}
+    'name': 'Neo',
+    'agents': ['Smith', 'Brown', 'Jones' ],
+    'spoon': false
+};
 
 mrouter.map(function () {
         // send and sendBody are different
         // root level URL
         this.root.bind(function (req, res) {
-            res.send("welcome to REST CRUD test, use /data");
+            res.send('welcome to REST CRUD test, use /data');
             });
 
         //GET request on /data
         this.get('/data').bind(function (req, res) {
-            console.log( "GET" );
+            console.log( 'GET' );
             res.sendBody(JSON.stringify(testData));
             });
 
         //GET request on a specific data field - /data/field
         this.get(/^\/data\/([A-Za-z0-9_]+)$/).bind(function (req, res, field) {
-            console.log( "GET " + field );
+            console.log( 'GET ' + field );
             res.sendBody(JSON.stringify(testData[field]));
             });
 
         //POST request on a specific data field - /data/field
         this.post(/^\/data\/([A-Za-z0-9_]+)$/).bind(function (req, res, field) {
-            console.log( "POST " + field );
+            console.log( 'POST ' + field );
             testData[field] = 'new value';
             res.sendBody(JSON.stringify(testData[field]));
             });
 
         //PUT request on a specific data field - /data/field
         this.put(/^\/data\/([A-Za-z0-9_]+)$/).bind(function (req, res, field) {
-            console.log( "PUT " + field );
+            console.log( 'PUT ' + field );
             testData[field] = 'new value';
             res.sendBody(JSON.stringify(testData[field]));
             });
 
         //DELETE request on a specific data field - /data/field
         this.del(/^\/data\/([A-Za-z0-9_]+)$/).bind(function (req, res, field) {
-                console.log( "DELETE " + field );
+                console.log( 'DELETE ' + field );
                 testData[field] = undefined; // not sure about this
-                res.sendBody("");
+                res.sendBody('');
                 });
 
         //POST request on a specific data field - /data/field/value
         this.post(/^\/data\/([A-Za-z0-9_]+)\/([A-Za-z0-9_]+)$/).bind(
                 function (req, res, field, value) {
-                console.log( "POST " + field + "=" + value );
+                console.log( 'POST ' + field + '=' + value );
                 testData[field] = value;
                 res.sendBody(JSON.stringify(testData[field]));
                 });
@@ -74,7 +76,7 @@ mrouter.map(function () {
         //PUT request on a specific data field - /data/field/value
         this.put(/\/data\/([A-Za-z0-9_]+)\/([A-Za-z0-9_]+)$/).bind(
                 function (req, res, field, value) {
-                console.log( "PUT " + field + "=" + value );
+                console.log( 'PUT ' + field + '=' + value );
                 testData[field] = value;
                 res.sendBody(JSON.stringify(testData[field]));
                 });
@@ -85,8 +87,8 @@ mrouter.map(function () {
 // create the server
 // to test interactively I used http-console 
 http.createServer(function (request, response) {
-        var body = "";
-        request.addListener('data', function (chunk) { body += chunk });
+        var body = '';
+        request.addListener('data', function (chunk) { body += chunk;});
         request.addListener('end', function () {
 
             mrouter.handle(request, body, function (result) {
